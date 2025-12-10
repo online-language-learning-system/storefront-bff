fc4aff690297c1715278cc0ed948a1f8768b8cc4
phccnguyenn <phccnguyenn2554@gmail.com>
2025-12-10 13:01:44 +0700

    [refactory] delete redundant part in SecurityConfig class

--- src/main/java/com/hub/storefront_bff/config/SecurityConfig.java
+++ src/main/java/com/hub/storefront_bff/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hub.storefront_bff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
